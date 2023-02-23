bac5c79d88d546c8d46364864da8acc962c17568
Sam Souza <innovatecodes@gmail.com>
2023-02-23 19:57:52 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -51,44 +51,56 @@ function rolagemSuave() {
     window.scrollTo({ top: topo, behavior: "smooth" });
     // Faz a rolagem suave até a posição do elemento
 }
-
 document.querySelector("#fa-angles-up").addEventListener("click", rolagemSuave);
 
 /* Navegação interna primária */
+const attr = {
+    fechar: "Fechar menu",
+    abrir: "Abrir menu",
+    expandido: "aria-expanded",
+    pressionado: "aria-pressed",
+    _etiqueta: "aria-label",
+    _falso: false,
+    _verdadeiro: true,
+}
+
 function menu(event) {
     const obterNavegacao = document.getElementById("navegacao-interna");
 
     if (event.type === "touchstart") {
         event.preventDefault();
     }
-
-    if (obterNavegacao.className != "hamburguer-ativo") {
+    
+    if (Boolean(obterNavegacao.className) === false && false == 0) {
         obterNavegacao.classList.add("hamburguer-ativo");
-        event.currentTarget.setAttribute("aria-expanded", true);
-        event.currentTarget.setAttribute("aria-pressed", true);
-        event.currentTarget.setAttribute("aria-label", "Fechar menu");
+        event.currentTarget.setAttribute(attr.expandido, attr._verdadeiro);
+        event.currentTarget.setAttribute(attr.pressionado, attr._verdadeiro);
+        event.currentTarget.setAttribute(attr._etiqueta, attr.fechar);
+
     } else {
         obterNavegacao.classList.remove("hamburguer-ativo")
-        event.currentTarget.setAttribute("aria-expanded", false);
-        event.currentTarget.setAttribute("aria-pressed", false);
-        event.currentTarget.setAttribute("aria-label", "Abrir menu");
+        event.currentTarget.setAttribute(attr.expandido, attr._falso);
+        event.currentTarget.setAttribute(attr.pressionado, attr._falso);
+        event.currentTarget.setAttribute(attr._etiqueta, attr.abrir);
     }
 }
-
 document.getElementById("hamburguer").addEventListener("click", menu);
 document.getElementById("hamburguer").addEventListener("touchstart", menu);
 
 // Fechar navegação ao clicar no link
-function linkAncora(event) {
-    menu(event);
-    document.getElementById("hamburguer").setAttribute("aria-expanded", false);
-    document.getElementById("hamburguer").setAttribute("aria-pressed", false);
-    document.getElementById("hamburguer").setAttribute("aria-label", "Abrir menu");
-}
+let links = document.querySelectorAll("a[href$='/'], a[href^='#'");
 
-document.querySelectorAll("a[href^='#'")[0].addEventListener("click", linkAncora);
-document.querySelectorAll("a[href^='#'")[1].addEventListener("click", linkAncora);
-document.querySelectorAll("a[href^='#'")[2].addEventListener("click", linkAncora);
+// Converte HTMLCollectionOf para Array
+links = Array.from(links);
+
+links.forEach(function (link) {
+    link.addEventListener("click", (event) => {
+        menu(event);
+        document.getElementById("hamburguer").setAttribute(attr.expandido, attr._falso);
+        document.getElementById("hamburguer").setAttribute(attr.pressionado, attr._falso);
+        document.getElementById("hamburguer").setAttribute(attr._etiqueta, attr.abrir);
+    })
+})
 
 // Efeito máquina de escrever 
 const obterH1 = document.querySelector("h1");
@@ -101,7 +113,6 @@ function digitando(element) {
         setTimeout(() => { element.innerHTML += letra; }, 100 * i);
     })
 }
-
 digitando(obterH1);
 
 // Ano atual e data de criação
@@ -109,23 +120,40 @@ const dataAtual = new Date();
 const anoAtual = dataAtual.getFullYear();
 const obterAnoAtual = document.getElementById("ano-atual");
 
-window.addEventListener("load", event => {
-    obterAnoAtual.innerHTML = anoAtual;
-})
-
-const dataCriacao = 2022;
 const obterTime = document.querySelector("time");
-obterTime.dateTime = dataCriacao;
-obterTime.textContent = dataCriacao;
+obterTime.dateTime = 2022;
+obterTime.textContent = 2022;
+
+function ano() {
+    return obterAnoAtual.innerHTML = anoAtual;
+}
+window.addEventListener("load", event => {
+    ano();
+})
 
 // Alterar tema
 const alterarModoTema = document.getElementById("alterar-tema");
 const obterControle = document.querySelector(".fa-circle-half-stroke");
+const raiz = document.querySelector(":root");
 
 if (sessionStorage.getItem("mode") == "dark") {
     alterarModoEscuro();
 } else {
     alterarModoClaro();
+}
+
+function alterarModoEscuro() {
+    raiz.classList.add("modo-claro"); //Adiciona a classe no html - :root
+    alterarModoTema.checked = true; //Configura o checkbox para (true)
+    sessionStorage.setItem("mode", "dark"); //Armazena um nome e valor para saber que o modo escuro está ativo
+    obterControle.classList.add("mover-controle");
+}
+
+function alterarModoClaro() {
+    raiz.classList.remove("modo-claro");  //Remove a classe no html - :root
+    alterarModoTema.checked = false; //Seta o checkbox para (false)
+    sessionStorage.setItem("mode", "light"); //Armazena um nome e valor para saber que o modo claro está ativo
+    obterControle.classList.remove("mover-controle");
 }
 
 //Se o estado do checkbox for mudado, executa a função
@@ -138,24 +166,9 @@ alterarModoTema.addEventListener("change", function () {
     }
 });
 
-function alterarModoEscuro() {
-    const raiz = document.querySelector(":root");
-    raiz.classList.add("modo-claro"); //Adiciona a classe no html - :root
-    alterarModoTema.checked = true; //Configura o checkbox para (true)
-    sessionStorage.setItem("mode", "dark"); //Armazena um nome e valor para saber que o modo escuro está ativo
-    obterControle.classList.add("mover-controle");
-}
-
-function alterarModoClaro() {
-    const raiz = document.querySelector(":root");
-    raiz.classList.remove("modo-claro");  //Remove a classe no html - :root
-    alterarModoTema.checked = false; //Seta o checkbox para (false)
-    sessionStorage.setItem("mode", "light"); //Armazena um nome e valor para saber que o modo claro está ativo
-    obterControle.classList.remove("mover-controle");
-}
-
 // Perguntas frequentes
 const ler = document.querySelectorAll(".ler");
+
 ler.forEach(function (elementoAtual) {
     elementoAtual.addEventListener("click", event => {
         elementoAtual.classList.toggle("girarAngulo");
